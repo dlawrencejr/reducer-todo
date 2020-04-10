efa93c6bed4745e75b0d227c47fa9a266f75be29
Dereck Lawrence Jr <dereck.lawrencejr@gmail.com>
2020-04-09 23:09:35 -0400

    MVP

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
+import {initialState,todoReducer} from './reducers/Reducers'
+import {TodoForm} from './components/TodoForm'
+import {TodoList} from './components/TodoList'
 import './App.css';
 
 function App() {
+    const [ state, dispatch ] = useReducer(todoReducer, initialState);
+    const addTodo = (input) => {
+      const newTodo = {
+        todo: input,
+        completed: false,
+        id: Math.random()
+      }
+      dispatch({ type: "ADD-TODO", payload: newTodo })
+    }
+
+    const handleComplete = (id) => {
+      dispatch({ type: "COMPLETE-TODO", payload: id })
+    }
+
+    const clearComplete = () => {
+      dispatch({ type: "CLEAR-COMPLETED" })
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList state={state} handleComplete={handleComplete} />
+     
+      <div className='todo-form'>
+
+      <TodoForm addTodo={addTodo} />
+      <br></br>
+      <button className='clear-button' onClick={(e) => {
+        e.preventDefault()
+        clearComplete()
+      }}>Clear Todo</button>
+      </div>
+     
     </div>
   );
 }
-
 export default App;
---
+++ reducer-todo/src/reducers/Reducers.js
@@ -0,0 +1,29 @@
+export const initialState = [{
+    task:'Learn about reducers',
+    completed: false,
+    id: Math.random()
+}]
+
+export const todoReducer = (state,action) =>{
+    switch(action.type){
+        case 'ADD-TODO':
+            return[
+                ...state,
+                action.payload
+            ]
+        case 'COMPLETE-TODO':
+            return state.map((item)=>{
+                return item.id === action.payload ?
+                {
+                    ...item,completed: !item.completed
+                }
+                : item
+            })
+        case 'CLEAR-COMPLETED':
+            return state.filter((item)=> !item.completed)
+
+        default:
+            return state
+    }
+    
+};
